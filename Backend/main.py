f2d72c65caf13745e8e7eb94ae4e5d0e1d74dff4
sohaib <souhaibemanah@gmail.com>
2024-02-10 12:30:46 +0100

    add get_execution_plan route

--- Backend/main.py
+++ Backend/main.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request, jsonify
 from sqlglot import parse
 from sqlglot.errors import ParseError
@@ -5,6 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/ValidateSQL', methods=['POST'])
 def validate_query():
@@ -14,7 +17,7 @@ def validate_query():
     try:
         # Utiliser sqlglot pour valider la syntaxe SQL
         parse(query)
-        
+
         # Si la syntaxe de la requête est valide, retourner un objet JSON avec le résultat
         return jsonify({'result': 'Valid SQL Query'})
     except ParseError as e:
@@ -25,8 +28,39 @@ def validate_query():
         context = e.errors[0]['start_context']
         highlight = e.errors[0]['highlight']
         end_context = e.errors[0]['end_context']
-        
-        return jsonify({'error': f"Invalid SQL Query: {error_description}. Error at line {line_number}, column {column_number}. Context: {context} {highlight} {end_context}"})
+
+        return jsonify({
+                           'error': f"Invalid SQL Query: {error_description}. Error at line {line_number}, column {column_number}. Context: {context} {highlight} {end_context}"})
+
+
+@app.route('/execution_plan')
+def execution_plan():  # put application's code here
+    body = request.json
+    query = body['query']
+    # !! sql injection !!
+    return {'execution_plan': get_execution_plan(query)}
+
+
+def get_execution_plan(query):
+    import oracledb
+
+    #  Connection string(todo : put it in a config file)
+    un = 'C##mohcine'
+    cs = '34.68.75.241/free'
+    pw = 'mohcine'
+
+    with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f"EXPLAIN PLAN FOR {query}")
+            cursor.execute(f"select * from table(dbms_xplan.display(null, null, 'SERIAL'))")
+            plan = cursor.fetchall()
+            # Process the plan into JSON format
+            json_plan = {'execution_plan': []}
+            for row in plan:
+                json_plan['execution_plan'].append(dict(zip([d[0] for d in cursor.description], row)))
+
+            return json.dumps(json_plan)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
